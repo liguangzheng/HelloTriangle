6ed2ca0b1b08d09f8144d2c864e5e23714166e82
liguangheng <liguangzhenghi@163.com>
2015-05-26 08:12:05 +0800

    first

---
+++ obj/local/mips/objs/Hello_Triangle/Common/Source/esTransform.o.d
@@ -0,0 +1,121 @@
+obj/local/mips/objs/Hello_Triangle/./Common/Source/esTransform.o: \
+ jni/./Common/Source/esTransform.c jni/Common/Include/esUtil.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/stdlib.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/cdefs.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/cdefs_elf.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/android/api-level.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/string.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/malloc.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/alloca.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/strings.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/stdint.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/_types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/_types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/_wchar_limits.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/posix_types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/stddef.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/compiler.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/posix_types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/sgidefs.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/kernel.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/sysmacros.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/memory.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/GLES3/gl3.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/GLES3/gl3platform.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/KHR/khrplatform.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/EGL/egl.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/EGL/eglplatform.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/android/native_window.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/android/rect.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/EGL/eglext.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/math.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/limits.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/limits.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/limits.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/internal_types.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/limits.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/syslimits.h \
+ E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/page.h
+
+jni/Common/Include/esUtil.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/stdlib.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/cdefs.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/cdefs_elf.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/android/api-level.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/string.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/malloc.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/alloca.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/strings.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/stdint.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/_types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/_types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/_wchar_limits.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/posix_types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/stddef.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/compiler.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/posix_types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/sgidefs.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/kernel.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/sysmacros.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/memory.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/GLES3/gl3.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/GLES3/gl3platform.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/KHR/khrplatform.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/EGL/egl.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/EGL/eglplatform.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/android/native_window.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/android/rect.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/EGL/eglext.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/math.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/limits.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/limits.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/linux/limits.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/internal_types.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/machine/limits.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/sys/syslimits.h:
+
+E:/develop/android/ndk/android-ndk-r10d/platforms/android-18/arch-mips/usr/include/asm/page.h:
